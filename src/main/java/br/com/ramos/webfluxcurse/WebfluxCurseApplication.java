c7653e4fcc2796709f94fce9ee388b6013a73a39
IldoRamos <ildoramosvieira@gmail.com>
2024-07-04 23:30:29 -0300

    iniciar projetos

---
+++ src/main/java/br/com/ramos/webfluxcurse/WebfluxCurseApplication.java
@@ -0,0 +1,13 @@
+package br.com.ramos.webfluxcurse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxCurseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxCurseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/ramos/webfluxcurse/WebfluxCurseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ramos.webfluxcurse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxCurseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
