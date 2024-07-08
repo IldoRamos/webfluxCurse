30c694238e1ce3c630219ef6e3fdb062950fa583
IldoRamos <ildoramosvieira@gmail.com>
2024-07-08 19:30:03 -0300

    add impl to update user contract

--- src/main/java/br/com/ramos/webfluxcurse/mapper/UserMapper.java
+++ src/main/java/br/com/ramos/webfluxcurse/mapper/UserMapper.java
@@ -3,10 +3,7 @@ package br.com.ramos.webfluxcurse.mapper;
 import br.com.ramos.webfluxcurse.entity.User;
 import br.com.ramos.webfluxcurse.model.request.UserRequest;
 import br.com.ramos.webfluxcurse.model.response.UserResponse;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.NullValueCheckStrategy;
-import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.*;
 
 @Mapper(
         componentModel = "spring",
@@ -17,6 +14,9 @@ public interface UserMapper {
 
     @Mapping(target = "id", ignore = true)
     User toEntity(final UserRequest userRequest);
+
+    @Mapping(target = "id", ignore = true)
+    User toEntity(final UserRequest userRequest, @MappingTarget final User user);
 
     UserResponse toRespose(final User entity);
 }
